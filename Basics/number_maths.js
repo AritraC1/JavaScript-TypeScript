2b788068bce40a27eb3d81ca67f719feb2466568
Aritra Chatterjee <ach142812@gmail.com>
2025-01-17 10:15:05 +0530

    Number and Maths

--- Basics/number_maths.js
+++ Basics/number_maths.js
@@ -2,7 +2,7 @@
 
 // +-----------------+ NUMBERS +-----------------+
 
-const score = 123.8966
+const score = 123.8966;
 console.log(score);
 console.log(score.toPrecision(3)); // returns string and the number range is from 1-21 inside the parenthesis
 
@@ -17,10 +17,30 @@ console.log(num.toLocaleString()); // By default us standard
 console.log(num.toLocaleString('en-IN')); // Indian value
 
 // Max Min Values in JS
-console.log(Number.MAX_VALUE)
-console.log(Number.MIN_VALUE)
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
 
 // +-----------------+ MATHS +-----------------+
 
-console.log(Math);
+console.log(Math); // Math libary is with JS by default
 
+console.log(Math.abs(-4)); // always negative to positive but vice-versa not possible
+console.log(Math.round(4.35)); // Round-off
+console.log(Math.ceil(4.2)); // ceil always chooses the top value
+console.log(Math.floor(4.9)); // floor always chooses the bottom value
+
+console.log(Math.sqrt(25));
+
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,3,6,8));
+
+console.log(Math.random()); // range: 0 to 1 by default
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+// MIN MAX Formula
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
