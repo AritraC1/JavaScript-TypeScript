7e8a77bf155275328e9102e21a21475929de56fc
Aritra Chatterjee <ach142812@gmail.com>
2025-01-17 12:51:21 +0530

    Date and Time

---
+++ Basics/date_time.js
@@ -0,0 +1,30 @@
+// DATES & TIME
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+console.log(myDate.toUTCString());
+console.log(typeof myDate); // Object
+
+console.log();
+
+let myCreatedDate = new Date(2023, 0, 23); // Months in JS start from 0 --> 0-Jan, 1-Feb, 2-Mar, .........
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.getTime());
+
+console.log();
+
+let date1 = new Date("2025-01-25"); 
+let date2 = new Date("01-14-2025"); 
+console.log(date1.toLocaleString());
+console.log(date2.toLocaleString());
+
+console.log();
+
+let myTimeStamp = Date.now(); // used in polls, quizes
+console.log(myTimeStamp);
+
