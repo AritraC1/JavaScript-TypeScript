acf8f475d84daae2965d091e9a07c8d89a2c18f9
Aritra Chatterjee <ach142812@gmail.com>
2025-05-17 23:19:27 +0530

    conversions

--- Basics/conversions.js
+++ Basics/conversions.js
@@ -23,7 +23,33 @@
 // "" => false
 // "abc" => true
 
-let num = 33
-let stringNum = String(num)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let num = 33
+// let stringNum = String(num)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+// Why string to number conversion is confusing?
+
+let val = 3
+let negValue = -val
+console.log(negValue);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+// Note: 
+// If string first then the rest is treated as a string -> console.log("1" + 2 + 2); // 122
+// If number is first and string is last then, first trated as number then string -> console.log(1 + 2 + "2"); // 32
+
+// Tricky Conversion (Avoid this)
+// console.log(+true);
+// console.log(+"");
+
+// let a,b,c
+// a = b = c = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
