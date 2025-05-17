2bb3120075f3cf794b7126c0352de8f8c890ee02
Aritra Chatterjee <ach142812@gmail.com>
2025-05-17 23:00:53 +0530

    conversions

---
+++ Basics/conversions.js
@@ -0,0 +1,29 @@
+// CONVERSIONS
+
+// let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber); // NaN - Not a number
+
+// console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" => true
+
+let num = 33
+let stringNum = String(num)
+console.log(stringNum);
+console.log(typeof stringNum);
