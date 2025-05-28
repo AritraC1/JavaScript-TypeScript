157dbbe7b3648bf40ee0bd18634d878c332991e4
Aritra Chatterjee <ach142812@gmail.com>
2025-05-28 11:52:57 +0530

    Arrays

---
+++ TS/Basics/arrays.ts
@@ -0,0 +1,22 @@
+// Arrays in TS
+
+const superHeroes: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+const allUsers: User[] = []
+
+superHeroes.push("Iron Man");
+heroPower.push(2);
+
+allUsers.push({name: "", isActive: true})
