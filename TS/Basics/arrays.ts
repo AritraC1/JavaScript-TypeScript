9d8e804cc3df30e45c21f8ffcc022f9e7ddbdf8e
Aritra Chatterjee <ach142812@gmail.com>
2025-05-29 11:26:21 +0530

    Arrays

--- TS/Basics/arrays.ts
+++ TS/Basics/arrays.ts
@@ -4,19 +4,19 @@ const superHeroes: string[] = [];
 // const heroPower: number[] = [];
 const heroPower: Array<number> = [];
 
-type User = {
-    name: string
-    isActive: boolean
-}
+// type User = {
+//     name: string
+//     isActive: boolean
+// }
 
 const MLModels: number[][] = [
     [255, 255, 255],
     []
 ]
 
-const allUsers: User[] = []
+// const allUsers: User[] = []
 
 superHeroes.push("Iron Man");
 heroPower.push(2);
 
-allUsers.push({name: "", isActive: true})
+// allUsers.push({name: "", isActive: true})
